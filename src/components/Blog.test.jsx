3d9a8028f72f5b727bc1aca7685df5c4faca36ff
Ezequiel Martin <ezequielmartin92@gmail.com>
2025-04-07 14:09:21 -0300

    Agrego test para probar que cuando hago click en el boton View me muestra toda la info del Blog

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import Blog from "./Blog"
 
 test("Titulo visible y lo demas oculto", () => {
@@ -21,4 +22,32 @@ test("Titulo visible y lo demas oculto", () => {
   element = screen.getByText("5")
   expect.soft(element).not.toBeVisible()
   
+})
+
+test("Clickeando el boton View muestro el contenido del Blog", async () => {
+  const blog = {
+    title: "Probando test de render title y lo demas oculto",
+    author: "Eze",
+    url: "test.com/render",
+    likes: 5,
+  }
+
+  // const mockHandler = vi.fn()
+
+  render(<Blog blog={blog}  />)
+  screen.debug()
+
+  const user = userEvent.setup()
+  const button = screen.getByText("View")
+  await user.click(button)
+  screen.debug()
+
+  let element = screen.getByText("Probando test de render title y lo demas oculto")
+  expect.soft(element).toBeVisible()
+  element = screen.getByText("Eze")
+  expect.soft(element).toBeVisible()
+  element = screen.getByText("test.com/render")
+  expect.soft(element).toBeVisible()
+  element = screen.getByText("5")
+  expect.soft(element).toBeVisible()
 })
